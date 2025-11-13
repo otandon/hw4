7341e57cd0fb53d1db887116f8e20decf1dbf44f
Om Tandon <otandon@usc.edu>
2025-11-13 22:31:58 +0000

    equal paths done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <climits>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +8,35 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+void findDepthRange(Node* root, int depth, 
+  int& min_depth, int& max_depth);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr) return true;
 
+    int min_depth = INT_MAX;
+    int max_depth = INT_MIN;
+
+    findDepthRange(root, 0, min_depth, max_depth);
+
+    return (min_depth == max_depth);
+
+}
+
+void findDepthRange(Node* root, int depth, int& min_depth, int& max_depth) {
+  if (root == nullptr) return;
+
+  // check if leaf
+  if (root -> left == nullptr && root -> right == nullptr) {
+    if (depth < min_depth) min_depth = depth;
+    if (depth > max_depth) max_depth = depth;
+    return;
+  }
+
+  // call on children if not leaf
+  findDepthRange(root -> left, depth + 1, min_depth, max_depth);
+  findDepthRange(root -> right, depth + 1, min_depth, max_depth);
 }
 
